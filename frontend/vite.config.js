ab1c12ee0557c11c0310a26b058b9e1cc1aeaead
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-07-12 12:49:59 +0530

    Ecom Edge Added as a Private Project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://ecomedge-backend.onrender.com",
-      "/uploads/": "https://ecomedge-backend.onrender.com",
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
     },
   },
 });
